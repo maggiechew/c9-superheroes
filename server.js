d0a9a6e08f8efbbd9a55db4dc41b3e7bfd10a5b0
Maggie Chew <maggie.chew.89@gmail.com>
2022-11-16 10:38:18 -0700

    Prepare for starting superheroes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import fetch from "node-fetch";
 const PORT = 5001;
 const app = express();
+
 app.get("/slow", (req, res) => {
   console.log("delay for 3 seconds");
   setTimeout(() => {
@@ -9,25 +10,7 @@ app.get("/slow", (req, res) => {
     res.send({ currentTime: seconds });
   }, 3000);
 });
-const weatherUrl = null;
-// ask George how to get your weatherURL for this to work :)
-const getWeather = async () => {
-  const response = await fetch(weatherUrl);
-  const data = await response.json();
-  return {
-    conditions: data.weather[0].description,
-    temperature: data.main.temp,
-  };
-  return { conditions: "cloudy", temperature: -23 };
-};
-app.get("/weather", async (req, res) => {
-  try {
-    const currentWeather = await getWeather();
-    res.send(currentWeather);
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).send();
-  }
-});
+
+
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
